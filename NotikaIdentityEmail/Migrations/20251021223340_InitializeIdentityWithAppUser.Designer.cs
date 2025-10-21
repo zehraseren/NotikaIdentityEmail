1f92e3d2374fc97892550308226fd4bbd85175e1
Zehra Seren <fatmazehraseren@gmail.com>
2025-10-22 01:42:52 +0300

    feat(migration): create migration for AppUser entity

--- NotikaIdentityEmail/Migrations/20251021223340_InitializeIdentityWithAppUser.Designer.cs
+++ NotikaIdentityEmail/Migrations/20251021223340_InitializeIdentityWithAppUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NotikaIdentityEmail.Context;
 
@@ -11,9 +12,11 @@ using NotikaIdentityEmail.Context;
 namespace NotikaIdentityEmail.Migrations
 {
     [DbContext(typeof(EmailContext))]
-    partial class EmailContextModelSnapshot : ModelSnapshot
+    [Migration("20251021223340_InitializeIdentityWithAppUser")]
+    partial class InitializeIdentityWithAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
