81846660b327fcca73387a0fb72361739d0b93d2
Zehra Seren <fatmazehraseren@gmail.com>
2025-10-22 01:04:58 +0300

    feat(DbContext): add EmailContext

---
+++ NotikaIdentityEmail/Context/EmailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace NotikaIdentityEmail.Context;
+
+public class EmailContext : IdentityDbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=Zehra;initial Catalog= NotikaEmailDb;integrated security=true;trust server certificate=true");
+    }
+}
