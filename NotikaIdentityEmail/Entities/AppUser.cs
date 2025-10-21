654feb997f8fd3f0b3ee69becdaedc43a132c3ef
Zehra Seren <fatmazehraseren@gmail.com>
2025-10-22 01:40:34 +0300

    feat(entity); add AppUser entity

---
+++ NotikaIdentityEmail/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NotikaIdentityEmail.Entities;
+
+public class AppUser : IdentityUser
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? City { get; set; }
+}
