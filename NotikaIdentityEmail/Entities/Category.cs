229fe498fc89c1cad8ea85668bc1f85ecd50d98a
Zehra Seren <fatmazehraseren@gmail.com>
2025-10-26 05:04:13 +0300

    feat(entity); add Category and Message entities

---
+++ NotikaIdentityEmail/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace NotikaIdentityEmail.Entities;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string CategoryIconUrl { get; set; }
+    public bool CategoryStatus { get; set; }
+}
---
+++ NotikaIdentityEmail/Entities/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace NotikaIdentityEmail.Entities;
+
+public class Message
+{
+    public int MessageId { get; set; }
+    public string SenderEmail { get; set; }
+    public string ReceiverEmail { get; set; }
+    public string Subject { get; set; }
+    public DateTime SendDate { get; set; }
+    public string MessageDetail { get; set; }
+    public bool IsRead { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
