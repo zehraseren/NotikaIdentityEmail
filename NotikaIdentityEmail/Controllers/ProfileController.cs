08a5d247b4c2e32dc2696f39e1f136f796f07c01
Zehra Seren <fatmazehraseren@gmail.com>
2025-10-26 07:31:59 +0300

    feat(profile): implement profile pages

---
+++ NotikaIdentityEmail/Controllers/ProfileController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.Controllers;
+public class ProfileController : Controller
+{
+    public IActionResult EditProfile()
+    {
+        return View();
+    }
+}
