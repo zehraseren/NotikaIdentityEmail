4ac60a50956e873a17cfe4f9ff554a4b746d82c9
Zehra Seren <fatmazehraseren@gmail.com>
2025-10-28 05:26:29 +0300

    refactor(profile): enhance profile update functionality

--- NotikaIdentityEmail/Controllers/ProfileController.cs
+++ NotikaIdentityEmail/Controllers/ProfileController.cs
@@ -1,10 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
+using Microsoft.AspNetCore.Identity;
+using NotikaIdentityEmail.Models.IdentityModels;
 
 namespace NotikaIdentityEmail.Controllers;
 public class ProfileController : Controller
 {
-    public IActionResult EditProfile()
+    private readonly UserManager<AppUser> _userManager;
+
+    public ProfileController(UserManager<AppUser> userManager)
     {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> EditProfile()
+    {
+        var values = await _userManager.FindByNameAsync(User.Identity.Name);
+
+        UserEditViewModel model = new();
+        model.Name = values.Name;
+        model.Surname = values.Surname;
+        model.UserName = values.UserName;
+        model.Email = values.Email;
+        model.PhoneNumber = values.PhoneNumber;
+        model.City = values.City;
+        model.ImageUrl = values.ImageUrl;
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditProfile(UserEditViewModel model)
+    {
+        if (model.Password == model.ConfirmPassword)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            user.Name = model.Name;
+            user.Surname = model.Surname;
+            user.UserName = model.UserName;
+            user.Email = model.Email;
+            user.PhoneNumber = model.PhoneNumber;
+            user.City = model.City;
+            user.ImageUrl = model.ImageUrl;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, model.Password);
+
+            await _userManager.UpdateAsync(user);
+        }
+
         return View();
     }
 }
---
+++ NotikaIdentityEmail/Models/IdentityModels/UserEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NotikaIdentityEmail.Models.IdentityModels;
+
+public class UserEditViewModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string City { get; set; }
+    public string? ImageUrl { get; set; }
+    public string PhoneNumber{ get; set; }
+}
