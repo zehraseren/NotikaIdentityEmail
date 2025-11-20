84ab3222184c51858cdfa72cb7e8cc72781df485
Zehra Seren <fatmazehraseren@gmail.com>
2025-11-20 21:25:40 +0300

    feat(entity); add Notification entity

---
+++ NotikaIdentityEmail/Entities/Notification.cs
@@ -0,0 +1,8 @@
+﻿namespace NotikaIdentityEmail.Entities;
+
+public class Notification
+{
+    public int NotificationId { get; set; }
+    public string Detail { get; set; }
+    public string ImageUrl { get; set; }
+}
