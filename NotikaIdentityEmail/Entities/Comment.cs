86a56737d84e869464a12518fd2a8fc53e8c85a6
Zehra Seren <fatmazehraseren@gmail.com>
2025-11-25 05:02:27 +0300

    feat(entity); add Comment entity

---
+++ NotikaIdentityEmail/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace NotikaIdentityEmail.Entities;
+
+public class Comment
+{
+    public int CommentId { get; set; }
+    public string CommentDetail { get; set; }
+    public DateTime CommentDate { get; set; }
+    public string CommentStatus { get; set; }
+    public string AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
+}
